81e781c77c52f75b8c356777b943b78050bfdaba
Nikolai <nikolaimoiseev01@gmail.com>
2025-09-17 00:55:19 +0300

    First commit

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Mercury</title>
+
+    <link rel="icon" type="image/png" href="fixed/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="fixed/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="fixed/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="fixed/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="fixed/favicon/site.webmanifest" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
